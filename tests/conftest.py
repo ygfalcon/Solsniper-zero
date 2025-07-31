5f88dcdef6d20f8a0c064460a07eeafb06edc012
ygfalcon <115580229+ygfalcon@users.noreply.github.com>
2025-07-31 17:48:16 -0400

    Add lightweight stubs for optional deps

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import os
 import sys
 import types
 import importlib.util
+import importlib.machinery
 
 if importlib.util.find_spec("aiohttp") is None:
     aiohttp_mod = types.ModuleType("aiohttp")
@@ -21,35 +22,11 @@ if importlib.util.find_spec("sentence_transformers") is None:
     st_mod.__spec__ = importlib.machinery.ModuleSpec("sentence_transformers", None)
     sys.modules.setdefault("sentence_transformers", st_mod)
 
-if importlib.util.find_spec("torch") is None:
-    torch_mod = types.ModuleType("torch")
-    torch_mod.__spec__ = importlib.machinery.ModuleSpec("torch", None)
-    torch_mod.__path__ = []
-    torch_mod.Tensor = object
-    torch_mod.cuda = types.SimpleNamespace(is_available=lambda: False)
-    torch_mod.backends = types.SimpleNamespace(mps=types.SimpleNamespace(is_available=lambda: False))
-    torch_mod.device = lambda *a, **k: types.SimpleNamespace(type="cpu")
-    torch_mod.load = lambda *a, **k: {}
-    class _NoGrad:
-        def __enter__(self):
-            pass
-        def __exit__(self, *exc):
-            pass
-    torch_mod.no_grad = lambda: _NoGrad()
-    torch_mod.tensor = lambda *a, **k: object()
-    sys.modules["torch"] = torch_mod
-    torch_nn = types.ModuleType("torch.nn")
-    torch_nn.__spec__ = importlib.machinery.ModuleSpec("torch.nn", None)
-    torch_nn.Module = object
-    sys.modules["torch.nn"] = torch_nn
-    torch_opt = types.ModuleType("torch.optim")
-    torch_opt.__spec__ = importlib.machinery.ModuleSpec("torch.optim", None)
-    sys.modules["torch.optim"] = torch_opt
-    tud = types.ModuleType("torch.utils.data")
-    tud.__spec__ = importlib.machinery.ModuleSpec("torch.utils.data", None)
-    tud.Dataset = object
-    tud.DataLoader = object
-    sys.modules["torch.utils.data"] = tud
+if importlib.util.find_spec("aiofiles") is None:
+    aiof_mod = types.ModuleType("aiofiles")
+    aiof_mod.__spec__ = importlib.machinery.ModuleSpec("aiofiles", None)
+    sys.modules.setdefault("aiofiles", aiof_mod)
+
 
 try:
     _gp_spec = importlib.util.find_spec("google.protobuf")
@@ -192,11 +169,238 @@ if importlib.util.find_spec("solana") is None:
     sys.modules.setdefault("solana.rpc.websocket_api", ws_mod)
 
 # Additional optional dependency stubs
-for _mod_name in ["cachetools", "sqlalchemy", "watchfiles", "psutil"]:
-    if importlib.util.find_spec(_mod_name) is None:
-        mod = types.ModuleType(_mod_name)
-        mod.__spec__ = importlib.machinery.ModuleSpec(_mod_name, None)
-        sys.modules.setdefault(_mod_name, mod)
+if importlib.util.find_spec("numpy") is None:
+    np_mod = types.ModuleType("numpy")
+    np_mod.__spec__ = importlib.machinery.ModuleSpec("numpy", None)
+
+    def _to_list(obj, fill=0.0):
+        if isinstance(obj, (list, tuple)):
+            return [list(o) if isinstance(o, (list, tuple)) else o for o in obj]
+        return [obj]
+
+    np_mod.array = lambda x, dtype=None: _to_list(x)
+    np_mod.asarray = lambda x, dtype=None: _to_list(x)
+
+    def _fill(shape, value):
+        if isinstance(shape, int):
+            return [value for _ in range(shape)]
+        if isinstance(shape, (list, tuple)) and len(shape) == 2:
+            return [[value for _ in range(shape[1])] for _ in range(shape[0])]
+        return [value]
+
+    np_mod.zeros = lambda shape, dtype=float: _fill(shape, 0.0)
+    np_mod.ones = lambda shape, dtype=float: _fill(shape, 1.0)
+    np_mod.full = lambda shape, val, dtype=None: _fill(shape, val)
+    sys.modules.setdefault("numpy", np_mod)
+
+if importlib.util.find_spec("cachetools") is None:
+    import collections
+    import time
+
+    ct_mod = types.ModuleType("cachetools")
+    ct_mod.__spec__ = importlib.machinery.ModuleSpec("cachetools", None)
+
+    class LRUCache(collections.OrderedDict):
+        def __init__(self, maxsize=128, *a, **kw):
+            self.maxsize = maxsize
+            super().__init__()
+
+        def __setitem__(self, key, value):
+            if key in self:
+                del self[key]
+            elif len(self) >= self.maxsize:
+                self.popitem(last=False)
+            super().__setitem__(key, value)
+
+    class TTLCache(LRUCache):
+        def __init__(self, maxsize=128, ttl=600):
+            super().__init__(maxsize)
+            self.ttl = ttl
+            self._exp = {}
+
+        def __setitem__(self, key, value):
+            super().__setitem__(key, value)
+            self._exp[key] = time.time() + self.ttl
+
+        def __getitem__(self, key):
+            if key in self._exp and self._exp[key] < time.time():
+                super().pop(key, None)
+                self._exp.pop(key, None)
+                raise KeyError(key)
+            return super().__getitem__(key)
+
+        def __contains__(self, key):
+            if key in self._exp and self._exp[key] < time.time():
+                super().pop(key, None)
+                self._exp.pop(key, None)
+                return False
+            return super().__contains__(key)
+
+    ct_mod.LRUCache = LRUCache
+    ct_mod.TTLCache = TTLCache
+    sys.modules.setdefault("cachetools", ct_mod)
+
+if importlib.util.find_spec("sqlalchemy") is None:
+    sa = types.ModuleType("sqlalchemy")
+    sa.__spec__ = importlib.machinery.ModuleSpec("sqlalchemy", None)
+
+    sa.Column = lambda *a, **k: None
+    sa.Integer = sa.Float = sa.String = sa.Text = sa.DateTime = object
+    sa.ForeignKey = lambda *a, **k: None
+    sa.create_engine = lambda *a, **k: None
+
+    class MetaData:
+        def create_all(self, *a, **k):
+            pass
+
+    sa.MetaData = MetaData
+
+    class Table:
+        def __init__(self, *a, **k):
+            pass
+
+    sa.Table = Table
+    sa.select = lambda *a, **k: None
+
+    ext = types.ModuleType("sqlalchemy.ext")
+    async_mod = types.ModuleType("sqlalchemy.ext.asyncio")
+    async_mod.create_async_engine = lambda *a, **k: None
+    async_mod.async_sessionmaker = lambda *a, **k: None
+    async_mod.AsyncSession = object
+    ext.asyncio = async_mod
+    sa.ext = ext
+
+    orm = types.ModuleType("sqlalchemy.orm")
+
+    def declarative_base(*a, **k):
+        return type("Base", (), {"metadata": MetaData()})
+
+    orm.declarative_base = declarative_base
+
+    class DummySession:
+        def __enter__(self):
+            return self
+
+        def __exit__(self, *exc):
+            pass
+
+        def add(self, *a, **k):
+            pass
+
+        def commit(self):
+            pass
+
+        def query(self, *a, **k):
+            return []
+
+    orm.sessionmaker = lambda *a, **k: lambda **kw: DummySession()
+    sa.orm = orm
+    sys.modules.setdefault("sqlalchemy", sa)
+    sys.modules.setdefault("sqlalchemy.ext", ext)
+    sys.modules.setdefault("sqlalchemy.ext.asyncio", async_mod)
+    sys.modules.setdefault("sqlalchemy.orm", orm)
+
+if importlib.util.find_spec("watchfiles") is None:
+    watch_mod = types.ModuleType("watchfiles")
+    watch_mod.__spec__ = importlib.machinery.ModuleSpec("watchfiles", None)
+
+    async def awatch(*a, **k):
+        if False:
+            yield None
+
+    watch_mod.awatch = awatch
+    sys.modules.setdefault("watchfiles", watch_mod)
+
+if importlib.util.find_spec("psutil") is None:
+    ps_mod = types.ModuleType("psutil")
+    ps_mod.__spec__ = importlib.machinery.ModuleSpec("psutil", None)
+    ps_mod.cpu_percent = lambda *a, **k: 0.0
+    ps_mod.virtual_memory = lambda: types.SimpleNamespace(percent=0.0)
+    sys.modules.setdefault("psutil", ps_mod)
+
+if importlib.util.find_spec("flask") is None:
+    flask_mod = types.ModuleType("flask")
+    flask_mod.__spec__ = importlib.machinery.ModuleSpec("flask", None)
+
+    class Request:
+        def __init__(self, *, method="GET", json=None, form=None, files=None):
+            self.method = method
+            self._json = json
+            self.form = form or {}
+            self.files = files or {}
+
+        def get_json(self):
+            return self._json
+
+    class Response:
+        def __init__(self, data, status=200):
+            self._data = data
+            self.status_code = status
+
+        def get_json(self):
+            return self._data
+
+    def jsonify(obj=None, **kw):
+        if obj is None:
+            obj = kw
+        return Response(obj)
+
+    def render_template_string(tmpl, **ctx):
+        return tmpl.format(**ctx)
+
+    class Flask:
+        def __init__(self, name, static_folder=None):
+            self.routes = {}
+
+        def route(self, path, methods=None):
+            methods = methods or ["GET"]
+
+            def decorator(func):
+                for m in methods:
+                    self.routes[(path, m)] = func
+                return func
+
+            return decorator
+
+        def test_client(self):
+            app = self
+
+            class Client:
+                def _call(self, method, path, json=None, data=None, content_type=None):
+                    form = {}
+                    files = {}
+                    if data and isinstance(data, dict):
+                        for k, v in data.items():
+                            if isinstance(v, tuple):
+                                fobj, fname = v
+                                setattr(fobj, "filename", fname)
+                                files[k] = fobj
+                            else:
+                                form[k] = v
+                    flask_mod.request = Request(method=method, json=json, form=form, files=files)
+                    func = app.routes.get((path, method))
+                    if not func:
+                        return Response({"message": "not found"}, 404)
+                    res = func()
+                    if isinstance(res, Response):
+                        return res
+                    if isinstance(res, tuple):
+                        return Response(res[0], res[1])
+                    return Response(res)
+
+                def get(self, path):
+                    return self._call("GET", path)
+
+                def post(self, path, json=None, data=None, content_type=None):
+                    return self._call("POST", path, json=json, data=data, content_type=content_type)
+
+            return Client()
+
+    flask_mod.Flask = Flask
+    flask_mod.jsonify = jsonify
+    flask_mod.request = Request()
+    flask_mod.render_template_string = render_template_string
+    sys.modules.setdefault("flask", flask_mod)
 
 # Ensure project root is in sys.path when running tests directly with 'pytest'
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
